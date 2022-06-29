12b90845a32b7e11a8498582513ef8500f64e3b8
Naufal Ghani <naufal.gramadhan@gmail.com>
2022-06-29 13:27:09 +0700

    Update MQTT_Subscribe_8266.ino
    Update fix kodingan, nuhuns

--- Lamp-Control-Simulation-Internet-of-Things/MQTT_Subscribe_8266/MQTT_Subscribe_8266.ino
+++ Lamp-Control-Simulation-Internet-of-Things/MQTT_Subscribe_8266/MQTT_Subscribe_8266.ino
@@ -7,7 +7,11 @@
 #define applicationName "Uhuy"   // Ganti dengan application name Antares yang telah dibuat
 #define deviceName "Ultrasonik"     // Ganti dengan device Antares yang telah dibuat
 
-const int relay1 = 14;
+const int relay1 = 4; //D1
+const int relay2 = 5; //D2
+const int relay3 = 2; //D4
+const int relay4 = 14; //D5
+const int relay5 = 12; //D6
 
 AntaresESP8266MQTT antares(ACCESSKEY);    // Buat objek antares
 
@@ -24,12 +28,20 @@ void callback(char topic[], byte payload[], unsigned int length) {
   Serial.println("Payload: " + antares.getPayload()); // Print payload
 
   // Print data-data yang telah didapatkan
-  Serial.println("Status Lampu: " + antares.getString("Status Lampu"));  
+ `Serial.println("Room 1: " + antares.getString("Room 1"));
+  Serial.println("Room 2: " + antares.getString("Room 2"));  
+  Serial.println("Room 3: " + antares.getString("Room 3"));  
+  Serial.println("Room 4: " + antares.getString("Room 4"));  
+  Serial.println("Room 5: " + antares.getString("Room 5"));
 }
 
 void setup() {
   Serial.begin(115200);     // Buka komunikasi serial dengan baudrate 115200
   pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
+  pinMode(relay4, OUTPUT);
+  pinMode(relay5, OUTPUT);
   
   antares.setDebug(true);   // Nyalakan debug. Set menjadi "false" jika tidak ingin pesan-pesan tampil di serial monitor
   antares.wifiConnection(WIFISSID,PASSWORD);  // Mencoba untuk menyambungkan ke WiFi
@@ -42,16 +54,52 @@ void loop() {
     akan dicoba untuk menyambungkan lagi
    */
   antares.checkMqttConnection();
-  String stat1 = antares.getString("Status Lampu");
-          delay(1000);
+ String stat1 = antares.getString("Room 1");
+  String stat2 = antares.getString("Room 2");
+  String stat3 = antares.getString("Room 3");
+  String stat4 = antares.getString("Room 4");
+  String stat5 = antares.getString("Room 5");
+  delay(1000);
+  
         if(stat1=="Nyala"){
           digitalWrite(relay1, LOW);
-          Serial.println("Nyala");
-          delay(1000);
+          Serial.println("Lampu 1 Nyala");
         }
-        if(stat1=="Mati"){
+          if(stat1=="Mati"){
           digitalWrite(relay1, HIGH);
-          Serial.println("Mati");
-          delay(1000);
+          Serial.println("Lampu 1 Mati");
         }
+          if(stat2=="Nyala"){
+          digitalWrite(relay2, LOW);
+          Serial.println("Lampu 2 Nyala");
+        }
+         if(stat2=="Mati"){
+          digitalWrite(relay2, HIGH);
+          Serial.println("Lampu 2 Mati");
+        }
+          if(stat3=="Nyala"){
+          digitalWrite(relay3, LOW);
+          Serial.println("Lampu 3 Nyala");
+        }
+        if(stat3=="Mati"){
+          digitalWrite(relay3, HIGH);
+          Serial.println("Lampu 3 Mati");
+        }
+          if(stat4=="Nyala"){
+          digitalWrite(relay4, LOW);
+          Serial.println("Lampu 4 Nyala");
+        }
+        if(stat4=="Mati"){
+          digitalWrite(relay4, HIGH);
+          Serial.println("Lampu 4 Mati");
+        }
+          if(stat5=="Nyala"){
+          digitalWrite(relay5, LOW);
+          Serial.println("Lampu 5 Nyala");
+        }
+        if(stat5=="Mati"){
+          digitalWrite(relay5, HIGH);
+          Serial.println("Lampu 5 Mati");
+        }
+     delay(1000);   
 }
